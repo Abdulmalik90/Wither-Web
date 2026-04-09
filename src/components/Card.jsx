ffee6f666f5508d8e7cc58da33e53790f802ea65
Abdulmalik Alkhalifh <bdalmlk832@gmail.com>
2026-04-09 13:56:00 +0300

    finish the logic of showing the temperature and weather

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,63 @@
+// react & css imports
+import { useEffect, useState } from "react";
 import "../styles/card.css"
+// MUI imports
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import CloudIcon from '@mui/icons-material/Cloud';
 import Button from '@mui/material/Button';
-export default function Card(){
+// other imports
+import axios from "axios";
+import { apiKey } from "../contexts/apiContext";
 
+
+let cancelAxios = null
+export default function Card(){
+    const [temp, setTemp] = useState({
+        current: null,
+        max: null,
+        min: null
+    })
+    const [weather, setWeather] = useState({description: "", icon: ""});
+
+    
+    // getting the wither by api
+    useEffect(()=>{
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=26.425699&lon=50.055164&appid=${apiKey}`, {
+            cancelToken: new axios.CancelToken((c)=>{
+                cancelAxios = c;
+            })
+        })
+        .then(function (response) {
+            // setting the temperature state
+            setTemp({current: Math.round(response.data.main.temp - 272.15),
+                max: Math.round(response.data.main.temp_max - 272.15),
+                min: Math.round(response.data.main.temp_min - 272.15)
+            })
+
+            // setting the weather condition state
+            setWeather({
+                description: response.data.weather[0].description,
+                icon: response.data.weather[0].icon
+            })
+
+            
+            
+            
+            console.log(response.data);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .finally(function () {
+            // always executed
+        });
+
+        return ()=> {
+            cancelAxios();
+        }
+    },[]);
     return (
         <Container maxWidth="sm">
             {/* Content Container */}
@@ -33,23 +86,24 @@ export default function Card(){
                             {/* Degree & Description */}
                             <div>
                                 {/* Temperature */}
-                                <div>
+                                <div style={{display: "flex"}}>
                                     <Typography variant="h1" style={{textAlign: "right"}} >
-                                        24
+                                        {temp.current}
                                     </Typography>
                                     {/* TODO: Sky Image */}
+                                    <img src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="" />
                                 </div>
                                 {/* // Temperature // */}
 
-                                <Typography variant="h6" style={{textAlign: "right"}} >
-                                        broken clouds
+                                <Typography variant="h6" style={{textAlign: "center"}} >
+                                        {weather.description}
                                 </Typography>
 
                                 {/* Min & Max */}
                                 <div style={{display:"flex", justifyContent: "space-between", alignItems: "center"}}>
-                                    <h5 >الصغرى: 20</h5>
+                                    <h5 >الصغرى: {temp.min}</h5>
                                     <h5 >|</h5>
-                                    <h5>الكبرى: 29</h5>
+                                    <h5>الكبرى: {temp.max}</h5>
                                 </div>
                             </div>
 
